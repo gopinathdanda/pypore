095af71d2472cfc5db27b988d656e05a90971deb
Will <wmparkin@gmail.com>
2014-04-09 18:13:59 -0400

    Mocked out imports with C extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -271,3 +271,31 @@ def setup(app):
 # import subprocess
 # p = subprocess.call(['./autogen_sphinx_apidoc.sh'])
 # os.chdir(WILL_CUR_DIR)
+
+# Mock out imports with C externs
+import sys
+
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['pypore', 'Cython', 'tables', 'PySide']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
